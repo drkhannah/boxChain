bf923bf75714e80d57a3e4ac9499e2cfc92f7676
Derek Hannah <drkhannah1@gmail.com>
2018-02-03 12:28:53 -0500

    created Blockchain struct

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
-}
+import (
+	"fmt"
+)
 
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1 BTC to Derek")
+	bc.AddBlock("Send 2 more BTC to Derek")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
